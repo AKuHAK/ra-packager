837c1a650180adc0129afedd2a324eaf85b8e0b9
AKuHAK <621640+AKuHAK@users.noreply.github.com>
2021-06-27 23:02:22 +0300

    lutro

--- package.sh
+++ package.sh
@@ -9,6 +9,10 @@
 ./cores/libretro-prboom.sh || { exit 1; }
 ./generate_retroarch.sh libretro-prboom prboom_libretro_ps2 || { exit 1; }
 
+# libretro-lutro
+./cores/libretro-lutro.sh || { exit 1; }
+./generate_retroarch.sh libretro-lutro lutro_libretro_ps2 || { exit 1; }
+
 #libretro-samples
 ./cores/libretro-samples.sh || { exit 1; }
 ./generate_retroarch.sh libretro-samples test_libretro_ps2 || { exit 1; }
